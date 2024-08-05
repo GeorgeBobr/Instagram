31c06a1abf54dca5f26ff5c3420d26ed369557a6
Georg <georgestoneix@gmail.com>
2024-08-05 18:45:19 +0600

    Настроил пути

--- instagram/accounts/urls.py
+++ instagram/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from accounts.views import RegisterView
+from accounts.views import RegisterView, UserProfileView
 
 app_name = 'accounts'
 
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', LoginView.as_view(template_name="register/login.html"), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='registration'),
-    # path('<int:pk>/profile/', ProfileView.as_view(), name='profile'),
+    path('profile/<int:pk>/', UserProfileView.as_view(), name='user_profile'),
     # path('<int:pk>/profile/edit/', UserChangeView.as_view(), name='profile_change'),
     # path('<int:pk>/profile/password-change/', UserPasswordChangeView.as_view(), name='password_change'),
 ]
--- instagram/webapp/urls.py
+++ instagram/webapp/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import HomeView, PostCreateView
-
+from .views import HomeView, PostCreateView, PostDetailView, like_post
+from .views_search import UserSearchView
 app_name = 'webapp'
 
 urlpatterns = [
     path('home/', HomeView.as_view(), name='home'),
     path('post/create/', PostCreateView.as_view(), name='post_create'),
+    path('search/', UserSearchView.as_view(), name='user_search'),
+
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('like/', like_post, name='like_post'),
 ]
+
