94322d87175a1421619e0477f828437ccdb52eae
Georg <georgestoneix@gmail.com>
2024-08-30 17:42:32 +0600

    Написал формы

--- instagram/webapp/forms.py
+++ instagram/webapp/forms.py
@@ -1,23 +1,17 @@
 from django import forms
-from .models import Post, Comment
-
+from .models import Post, Comment, Like
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['image', 'description']
 
-class LikeForm(forms.Form):
-    post_id = forms.IntegerField(widget=forms.HiddenInput())
-
-
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
-        widgets = {
-            'content': forms.Textarea(attrs={'rows': 3}),
-        }
 
-class UserSearchForm(forms.Form):
-    query = forms.CharField(max_length=100, required=False, label='Search')
+class LikeForm(forms.ModelForm):
+    class Meta:
+        model = Like
+        fields = []
