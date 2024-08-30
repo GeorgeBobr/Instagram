249fb24e203336f4f511052b0fd8d51fe9bbf9ea
Georg <georgestoneix@gmail.com>
2024-08-30 17:05:00 +0600

    Написал вьюшки

--- instagram/accounts/views.py
+++ instagram/accounts/views.py
@@ -6,7 +6,6 @@ from accounts.forms import MyUserCreationForm
 from django.core.paginator import Paginator
 from django.contrib.auth.views import PasswordChangeView
 from django.contrib.auth.decorators import login_required
-
 from accounts.models import CustomUser
 from webapp.models import Post
 
@@ -16,7 +15,7 @@ User = get_user_model()
 class RegisterView(CreateView):
     model = get_user_model()
     form_class = MyUserCreationForm
-    template_name = 'register/registration.html'
+    template_name = 'registration.html'
 
     def form_valid(self, form):
         user = form.save()
@@ -32,28 +31,12 @@ class RegisterView(CreateView):
 
         return next_page
 
-
 class UserProfileView(DetailView):
     model = CustomUser
-    template_name = 'user/user_profile.html'
+    template_name = 'profile.html'
     context_object_name = 'user'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['posts'] = Post.objects.filter(author=self.object).order_by('-created_at')
         return context
-
-
-@login_required
-def follow_user(request):
-    if request.method == 'POST':
-        user_id = request.POST.get('user_id')
-        user_to_follow = get_object_or_404(User, id=user_id)
-        if user_to_follow != request.user and not request.user.following.filter(id=user_to_follow.id).exists():
-            request.user.following.add(user_to_follow)
-            user_to_follow.followers.add(request.user)
-            request.user.following_count = request.user.following.count()
-            user_to_follow.follower_count = user_to_follow.followers.count()
-            request.user.save()
-            user_to_follow.save()
-    return redirect('accounts:user_profile', pk=user_id)
---
+++ instagram/api/views.py
@@ -0,0 +1,79 @@
+# views.py
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import Post, Like, Comment
+from .serializers import PostSerializer, LikeSerializer, CommentSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_update(self, serializer):
+        if serializer.instance.user != self.request.user:
+            raise permissions.PermissionDenied("You do not have permission to edit this post.")
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        if instance.user != self.request.user:
+            raise permissions.PermissionDenied("You do not have permission to delete this post.")
+        instance.delete()
+
+class LikeViewSet(viewsets.ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+
+    def get_permissions(self):
+        if self.action in ['create', 'destroy']:
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
+
+    def create(self, request, *args, **kwargs):
+        post_id = request.data.get('post')
+        post = Post.objects.get(id=post_id)
+        like, created = Like.objects.get_or_create(user=request.user, post=post)
+        if not created:
+            return Response({'detail': 'Already liked'}, status=status.HTTP_400_BAD_REQUEST)
+        post.like_count = post.likes.count()
+        post.save()
+        return Response(self.get_serializer(like).data, status=status.HTTP_201_CREATED)
+
+    def destroy(self, request, *args, **kwargs):
+        post_id = request.data.get('post')
+        post = Post.objects.get(id=post_id)
+        like = Like.objects.get(user=request.user, post=post)
+        like.delete()
+        post.like_count = post.likes.count()
+        post.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_update(self, serializer):
+        if serializer.instance.user != self.request.user:
+            raise permissions.PermissionDenied("You do not have permission to edit this comment.")
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        if instance.user != self.request.user:
+            raise permissions.PermissionDenied("You do not have permission to delete this comment.")
+        instance.delete()
+    
