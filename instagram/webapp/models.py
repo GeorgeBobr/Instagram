68c98f7b2fe46f706985f50a4fa5e9663e880622
Georg <georgestoneix@gmail.com>
2024-08-30 17:43:21 +0600

    Добавил модельки лайков и комментов

--- instagram/webapp/models.py
+++ instagram/webapp/models.py
@@ -1,36 +1,27 @@
-# webapp/models.py
-
 from django.db import models
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    image = models.ImageField(upload_to='posts/', blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    image = models.ImageField(upload_to='posts/')
+    description = models.TextField(blank=True, null=True)
+    like_count = models.IntegerField(default=0)
+    comment_count = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
-
-    @property
-    def like_count(self):
-        return self.likes.count()
 
     def __str__(self):
-        return self.title
+        return f'Post by {self.user.username} - {self.created_at}'
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ['created_at']
-
-    def __str__(self):
-        return f'Comment by {self.author} on {self.post}'
